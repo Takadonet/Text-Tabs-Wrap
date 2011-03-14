d825e24061841221a9045e606423a7dac2a1aded
Anthony Parsons <flussence+git@gmail.com>
2011-03-14 02:06:51 +0000

    Update the POD to Pod in Text::Wrap
    Some correctness-editing was done too

--- lib/Text/Wrap.pm
+++ lib/Text/Wrap.pm
@@ -1,6 +1,43 @@
 module Text::Wrap;
 use Text::Tabs;
 
+=begin pod
+
+=head1 NAME
+
+Text::Wrap -- line wrapping to form simple paragraphs
+
+=head1 SYNOPSIS
+
+=begin code
+    use Text::Wrap;
+
+    $initial_tab = "\t";    # Tab before first line
+    $subsequent_tab = "";   # All other lines flush left
+
+    $lines = wrap($initial_tab, $subsequent_tab, @text);
+    @paragraphs = fill($initial_tab, $subsequent_tab, @text);
+
+    $Text::Wrap::huge = 'wrap';
+    $Text::Wrap::columns = %*ENV<COLUMNS> // 80;
+    print wrap('', '', @text);  # Format text for terminal output
+=end code
+
+=head1 DESCRIPTION
+
+Text::Wrap provides two functions for wrapping text:
+
+C<wrap()> is a very simple paragraph formatter. It formats a single paragraph at a time by breaking
+lines at word boundaries. Indentation is controlled for the first line and all subsequent lines
+independently, via C<$para-indent> and C<$body-indent>.
+
+C<fill()> is a simple multi-paragraph formatter. It breaks text into paragraphs on blank-line
+boundaries, each paragraph is passed to C<wrap()> and the result is joined back together.
+
+Both C<wrap()> and C<fill()> return a single string.
+
+=end pod
+
 our $break = rx{\s};
 our $huge = 'wrap';             # How to handle long words; valid values are <wrap die overflow>
 our #`[Int] $columns = 76;      # Screen width
@@ -113,142 +150,69 @@ sub fill(Str $para-indent, Str $body-indent, *@raw) returns Str is export {
         .join($para-indent eq $body-indent ?? "\n\n" !! "\n");
 }
 
-# =head1 NAME
-
-# Text::Wrap - line wrapping to form simple paragraphs
-
-# =head1 SYNOPSIS
-
-# B<Example 1>
-
-#   use Text::Wrap;
-
-#   $initial_tab = "\t";    # Tab before first line
-#   $subsequent_tab = "";   # All other lines flush left
-
-#   print wrap($initial_tab, $subsequent_tab, @text);
-#   print fill($initial_tab, $subsequent_tab, @text);
-
-#   $lines = wrap($initial_tab, $subsequent_tab, @text);
-
-#   @paragraphs = fill($initial_tab, $subsequent_tab, @text);
-
-# B<Example 2>
-
-#   use Text::Wrap qw(wrap $columns $huge);
-
-#   $columns = 132;     # Wrap at 132 characters
-#   $huge = 'die';
-#   $huge = 'wrap';
-#   $huge = 'overflow';
-
-# B<Example 3>
-
-#   use Text::Wrap;
-
-#   $Text::Wrap::columns = 72;
-#   print wrap('', '', @text);
-
-# =head1 DESCRIPTION
-
-# C<Text::Wrap::wrap()> is a very simple paragraph formatter.  It formats a
-# single paragraph at a time by breaking lines at word boundaries.
-# Indentation is controlled for the first line (C<$initial_tab>) and
-# all subsequent lines (C<$subsequent_tab>) independently.  Please note:
-# C<$initial_tab> and C<$subsequent_tab> are the literal strings that will
-# be used: it is unlikely you would want to pass in a number.
-
-# Text::Wrap::fill() is a simple multi-paragraph formatter.  It formats
-# each paragraph separately and then joins them together when it's done.  It
-# will destroy any whitespace in the original text.  It breaks text into
-# paragraphs by looking for whitespace after a newline.  In other respects
-# it acts like wrap().
-
-# Both C<wrap()> and C<fill()> return a single string.
-
-# =head1 OVERRIDES
-
-# C<Text::Wrap::wrap()> has a number of variables that control its behavior.
-# Because other modules might be using C<Text::Wrap::wrap()> it is suggested
-# that you leave these variables alone!  If you can't do that, then
-# use C<local($Text::Wrap::VARIABLE) = YOURVALUE> when you change the
-# values so that the original value is restored.  This C<local()> trick
-# will not work if you import the variable into your own namespace.
-
-# Lines are wrapped at C<$Text::Wrap::columns> columns (default value: 76).
-# C<$Text::Wrap::columns> should be set to the full width of your output
-# device.  In fact, every resulting line will have length of no more than
-# C<$columns - 1>.
-
-# It is possible to control which characters terminate words by
-# modifying C<$Text::Wrap::break>. Set this to a string such as
-# C<'[\s:]'> (to break before spaces or colons) or a pre-compiled regexp
-# such as C<qr/[\s']/> (to break before spaces or apostrophes). The
-# default is simply C<'\s'>; that is, words are terminated by spaces.
-# (This means, among other things, that trailing punctuation  such as
-# full stops or commas stay with the word they are "attached" to.)
-# Setting C<$Text::Wrap::break> to a regular expression that doesn't
-# eat any characters (perhaps just a forward look-ahead assertion) will
-# cause warnings.
-
-# Beginner note: In example 2, above C<$columns> is imported into
-# the local namespace, and set locally.  In example 3,
-# C<$Text::Wrap::columns> is set in its own namespace without importing it.
-
-# C<Text::Wrap::wrap()> starts its work by expanding all the tabs in its
-# input into spaces.  The last thing it does it to turn spaces back
-# into tabs.  If you do not want tabs in your results, set
-# C<$Text::Wrap::unexpand> to a false value.  Likewise if you do not
-# want to use 8-character tabstops, set C<$Text::Wrap::tabstop> to
-# the number of characters you do want for your tabstops.
-
-# If you want to separate your lines with something other than C<\n>
-# then set C<$Text::Wrap::separator> to your preference.  This replaces
-# all newlines with C<$Text::Wrap::separator>.  If you just want to
-# preserve existing newlines but add new breaks with something else, set
-# C<$Text::Wrap::separator2> instead.
-
-# When words that are longer than C<$columns> are encountered, they
-# are broken up.  C<wrap()> adds a C<"\n"> at column C<$columns>.
-# This behavior can be overridden by setting C<$huge> to
-# 'die' or to 'overflow'.  When set to 'die', large words will cause
-# C<die()> to be called.  When set to 'overflow', large words will be
-# left intact.
-
-# Historical notes: 'die' used to be the default value of
-# C<$huge>.  Now, 'wrap' is the default value.
-
-# =head1 EXAMPLES
-
-# Code:
-
-#   print wrap("\t","",<<END);
-#   This is a bit of text that forms
-#   a normal book-style indented paragraph
-#   END
-
-# Result:
-
-#   "   This is a bit of text that forms
-#   a normal book-style indented paragraph
-#   "
-
-# Code:
-
-#   $Text::Wrap::columns=20;
-#   $Text::Wrap::separator="|";
-#   print wrap("","","This is a bit of text that forms a normal book-style paragraph");
-
-# Result:
-
-#   "This is a bit of|text that forms a|normal book-style|paragraph"
-
-# =head1 SEE ALSO
-
-# For wrapping multi-byte characters: L<Text::WrapI18N>.
-# For more detailed controls: L<Text::Format>.
-
 =begin pod
+
+=head1 GLOBALS
+
+Text::Wrap has a number of package-global vars for backwards-compatibility with Perl 5; we'll add
+a more sensible function param way of setting these in the near future and remove these vars.
+
+Lines are wrapped at C<$Text::Wrap::columns> columns (default value: 76). C<$Text::Wrap::columns>
+should be set to the full width of your output device. The actual text width will normally be 1
+less than this as C<wrap()> reserves one character for the C<\n> ending each line. The exception to
+this is when the combined indentation and column width would result in no space for text otherwise.
+
+It is possible to define how "words" are separated by modifying C<$Text::Wrap::break> (default
+value: C<rx/\s/>). Set this to any valid regex, such as C<rx/\s|':'/> to break before spaces or
+colons, C<rx/\s|"'"/> to break before spaces or apostrophes and so on. The default is simply
+to split on whitespace. (This means, among other things, that trailing punctuation such as full
+stops or commas stay with the word they are "attached" to.) Setting C<$Text::Wrap::break> to a
+regular expression that doesn't eat any characters (perhaps just a forward look-ahead assertion)
+will likely cause bad things to happen.
+
+C<wrap()> starts its work by expanding all tabs in its input into spaces. The last thing it does is
+to turn spaces back into tabs. If you do not want tabs in the output, set C<$Text::Wrap::unexpand>
+to a false value. Likewise if you do not want to use 8-character tabstops, pass a different numeric
+C<:$tabstop> value to C<wrap()>.
+
+=for comment
+N.B. The logic of the separator vars is horribly convoluted. This part of the module may change.
+
+C<$Text::Wrap::separator> defines the logical line delimiter for output. By default this is
+C<\n>. C<$Text::Wrap::separator2> is similar, but when set it overrides the value of C<$separator>
+and existing newline characters in the input are preserved.
+
+When words that are longer than C<$columns> are encountered, by default they are broken up.
+C<wrap()> inserts a line break in the word at column C<$columns>. This behavior can be overridden by
+setting C<$Text::Wrap::huge> to 'die' or to 'overflow'. When set to 'die', large words will cause
+C<die()> to be called. When set to 'overflow', large words will be left intact.
+
+Historical notes: 'die' used to be the default value of C<$huge>. Now, 'wrap' is the default value.
+
+=head1 EXAMPLES
+
+Code:
+
+  print wrap("\t","",q:to<END>);
+  This is a bit of text that forms
+  a normal book-style indented paragraph
+  END
+
+Result:
+
+  "   This is a bit of text that forms
+  a normal book-style indented paragraph
+  "
+
+Code:
+
+  $Text::Wrap::columns=20;
+  $Text::Wrap::separator="|";
+  print wrap("","","This is a bit of text that forms a normal book-style paragraph");
+
+Result:
+
+  "This is a bit of|text that forms a|normal book-style|paragraph"
 
 =head1 AUTHORS
 
